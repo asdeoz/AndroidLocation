0c5cb93d757507a86f66d2a6fb63582a11f63b2d
asdeoz <asdeoz@gmail.com>
2012-10-02 18:18:47 +0200

    First commit
    Commit of all the changes

---
+++ AndroidLocationService/ILocationService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AndroidLocationService
+{
+    [ServiceContract(Namespace= "http://services.example.com")]
+    public interface ILocationService
+    {
+        [OperationContract]
+        [WebGet(
+            UriTemplate = "GetPlates",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        IList<string> GetPlates();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "GetVehicle/{plate}",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        Vehicle GetVehicle(string plate);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "POST",
+            UriTemplate = "SaveVehicle",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        void SaveVehicle(Vehicle vehicle);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "GetPlate",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        string GetPlate();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "ToUpper/{toUpper}",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        string ToUpper(string toUpper);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Writer/{text}",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        void Writer(string text);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Reader",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        string Reader();
+
+    }
+
+    [DataContract]
+    public class Vehicle
+    {
+        [DataMember(Name = "year")]
+        public int Year { get; set; }
+
+        [DataMember(Name = "plate")]
+        public string Plate { get; set; }
+
+        [DataMember(Name = "make")]
+        public string Make { get; set; }
+
+        [DataMember(Name = "model")]
+        public string Model { get; set; }
+    }
+
+}
---
+++ AndroidLocationService/LocationService.svc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+
+namespace AndroidLocationService
+{
+    public class LocationService : ILocationService
+    {
+
+        #region Miembros de ILocationService
+
+        public IList<string> GetPlates()
+        {
+            List<string> lista = new List<string>();
+
+            lista.Add("aaa");
+            lista.Add("bbb");
+            lista.Add("ccc");
+
+            return lista;
+        }
+
+        public Vehicle GetVehicle(string plate)
+        {
+            Vehicle v = new Vehicle();
+
+            v.Plate = plate;
+
+            return v;
+        }
+
+        public void SaveVehicle(Vehicle vehicle)
+        {
+            
+        }
+
+        public string GetPlate()
+        {
+            return "aaa";
+        }
+
+        public string ToUpper(string toUpper)
+        {
+            return toUpper.ToUpper();
+        }
+
+        public void Writer(string text)
+        {
+            FileStream fs = new FileStream("|DataDirectory|/location.txt", FileMode.OpenOrCreate, FileAccess.Write);
+
+            TextWriter tw = new StreamWriter(fs);
+
+            tw.WriteLine(text);
+
+            tw.Close();
+        }
+
+        public string Reader()
+        {
+            FileStream fs = new FileStream("location.txt", FileMode.Open, FileAccess.Read);
+
+            TextReader tr = new StreamReader(fs);
+
+            string line = tr.ReadLine();
+
+            tr.Close();
+
+            return line;
+        }
+
+        #endregion
+    }
+}
